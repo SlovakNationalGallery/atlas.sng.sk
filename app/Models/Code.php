04d1b161b0f733e80f75060d8229a483a23a8eee
Igor Rjabinin <igor.rjabinin@gmail.com>
2022-01-27 18:03:48 +0100

    add Code model

---
+++ app/Models/Code.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Code extends Model
+{
+    use HasFactory;
+
+
+    const ROWS = 3;
+    const COLS = 3;
+
+    // codes are stored in DB in decimal value, e.g. 000101010 -> 42
+    public function getCodeAttribute($value)
+    {
+        return str_pad(decbin($value), self::ROWS * self::COLS, '0', STR_PAD_LEFT);
+    }
+
+    public function setCodeAttribute($value)
+    {
+        $this->attributes['code'] = bindec($value);
+    }
+}
---
+++ database/migrations/2022_01_27_163428_create_codes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('codes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('code')->unique();
+            $table->string('item_id');
+            $table->integer('counter')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('codes');
+    }
+}
