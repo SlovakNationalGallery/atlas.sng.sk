d18b0db5674f7cfcd788c8da88598a805f510f9f
Igor Rjabinin <igor.rjabinin@gmail.com>
2022-01-28 11:17:31 +0100

    add tailwind

---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+        <title>Záložky SNG</title>
+    </head>
+    <body>
+        <h1 class="text-3xl font-bold underline">
+            Záložky SNG
+        </h1>
+        <script src="{{ mix('/js/app.js') }}"></script>
+    </body>
+</html>
