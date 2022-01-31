3d8e8f1b4e7769e4d6def6aa041b3e57ff50362e
Igor Rjabinin <igor.rjabinin@gmail.com>
2022-01-31 21:15:27 +0100

    add RectangleButton component + icons in bottom navigation

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,19 +1,32 @@
 <template>
-    <div>
-        <h1>Záložky SNG</h1>
+    <div class="w-100 border-top-black">
+        <h1 class="p-5 font-bold text-lg">Záložky SNG</h1>
     </div>
     <div>
         <div class="max-w-screen-lg mx-auto p-4">
           <div class="grid grid-cols-3 gap-0 mt-12 border-collapse border border-black">
-            <Button v-for="index in 9" :key="index"></Button>
+            <CircleButton v-for="index in 9" :key="index"></CircleButton>
             <div class="w-full border border-black">
-                <div class="p-3 text-center">🌐</div>
+                <RectangleButton>
+                    <svg class="mx-auto" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M16 28C22.6274 28 28 22.6274 28 16C28 9.37258 22.6274 4 16 4C9.37258 4 4 9.37258 4 16C4 22.6274 9.37258 28 16 28Z" stroke-width="2.08" stroke-miterlimit="10"/>
+                    <path d="M4.68359 12H27.3167" stroke-width="2.08" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M4.68359 20H27.3166" stroke-width="2.08" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M16 27.678C18.7614 27.678 21 22.4496 21 16.0001C21 9.55063 18.7614 4.32227 16 4.32227C13.2386 4.32227 11 9.55063 11 16.0001C11 22.4496 13.2386 27.678 16 27.678Z" stroke-width="2.08" stroke-miterlimit="10"/>
+                    </svg>
+                </RectangleButton>
             </div>
             <div class="w-full border border-black">
-                <div class="p-3 text-center">CHECK</div>
+                <RectangleButton>Check</RectangleButton>
             </div>
             <div class="w-full border border-black">
-                <div class="p-3 text-center">?</div>
+                <RectangleButton>
+                    <svg class="mx-auto" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M16 28C22.6274 28 28 22.6274 28 16C28 9.37258 22.6274 4 16 4C9.37258 4 4 9.37258 4 16C4 22.6274 9.37258 28 16 28Z" stroke-width="2.08" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M16 24C16.8284 24 17.5 23.3284 17.5 22.5C17.5 21.6716 16.8284 21 16 21C15.1716 21 14.5 21.6716 14.5 22.5C14.5 23.3284 15.1716 24 16 24Z" fill="black"/>
+                    <path d="M16 18V17C16.6922 17 17.3689 16.7947 17.9445 16.4101C18.5201 16.0256 18.9687 15.4789 19.2336 14.8394C19.4985 14.1999 19.5678 13.4961 19.4327 12.8172C19.2977 12.1383 18.9644 11.5146 18.4749 11.0251C17.9854 10.5356 17.3618 10.2023 16.6828 10.0673C16.0039 9.9322 15.3002 10.0015 14.6606 10.2664C14.0211 10.5313 13.4744 10.9799 13.0899 11.5555C12.7053 12.1311 12.5 12.8078 12.5 13.5" stroke-width="2.08" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </RectangleButton>
             </div>
           </div>
         </div>
@@ -21,9 +34,10 @@
 </template>
 
 <script>
-    import Button from './components/Button.vue'
+    import CircleButton from './components/CircleButton.vue'
+    import RectangleButton from './components/RectangleButton.vue'
 
     export default {
-        components: { Button }
+        components: { CircleButton, RectangleButton }
     }
 </script>
---
+++ resources/js/components/RectangleButton.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="h-full p-3 text-center stroke-black active:bg-black active:text-white active:stroke-white">
+      <slot></slot>
+    </div>
+</template>
