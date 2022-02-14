60c8f0543f622a0bc645d216f65e781071422484
Igor Rjabinin <igor.rjabinin@gmail.com>
2022-02-14 23:09:15 +0100

    bottom dialog to add / skip item to collection

--- resources/js/views/Detail.vue
+++ resources/js/views/Detail.vue
@@ -29,12 +29,30 @@
     </div>
     <div class="bg-gray-200 h-48 border-black border-t-2">
         <img class="h-full object-cover w-full" :src="getImage(item, 800)" v-if="item">
-        <!-- preview image -->
     </div>
     <div class="h-full border-black border-t-2 p-4" v-if="item">
         <h2 class="text-xl font-bold">{{ item.title }}</h2>
         <h3 class="">{{ formatName(item.author[0]) }} · {{ item.dating }}</h3>
         <div class="py-4 text-sm" v-html="item.description"></div>
+    </div>
+    <div class="bg-gray-200 w-full h-48 fixed bottom-0">
+        <div class="p-4">
+            <div class="flex items-center">
+                <svg class="" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M11.25 11.25H12V16.5H12.75" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M11.8125 9C12.4338 9 12.9375 8.49632 12.9375 7.875C12.9375 7.25368 12.4338 6.75 11.8125 6.75C11.1912 6.75 10.6875 7.25368 10.6875 7.875C10.6875 8.49632 11.1912 9 11.8125 9Z" fill="black"/>
+                </svg>
+                <div class="pl-2 font-bold">More info later. Keep collecting!</div>
+            </div>
+            <div class="py-4 text-sm">
+                You’ll see more details when you share the collection and view it from another device. Keep collecting!
+            </div>
+            <div class="flex space-x-4">
+                <button class="border-black border-2 py-4 w-full bg-white font-bold">Don’t add</button>
+                <button class="border-black border-2 py-4 w-full bg-black text-white font-bold">Add new</button>
+            </div>
+        </div>
     </div>
 </template>
 
