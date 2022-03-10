3ccf055b57d91a6a568a2c65bd5eb9d4803b86a1
Igor Rjabinin <igor.rjabinin@gmail.com>
2022-03-10 15:56:09 +0100

    add ThumbRow component

---
+++ resources/js/components/ThumbRow.vue
@@ -0,0 +1,18 @@
+<template>
+    <router-link :to="'/detail/' + itemId">
+        <div class="flex my-4">
+            <div class="h-24 w-24 border-black border-2 flex items-center justify-center">
+                <svg class="w-9 h-9" fill="none" xmlns="http://www.w3.org/2000/svg"></svg>
+            </div>
+            <div class="py-2 px-4">
+                <h2 class="text-base pb-1">{{ itemId }}</h2>
+                <div class="text-sm text-gray-dark">Miloš Alexander Bazovský  a ...</div>
+            </div>
+        </div>
+    </router-link>
+</template>
+
+<script setup>
+    import { computed, ref } from "vue"
+    const props = defineProps( [ 'itemId' ] )
+</script>
--- resources/js/views/Collection.vue
+++ resources/js/views/Collection.vue
@@ -3,17 +3,7 @@
 
     <div class="h-full border-black p-4 border-t-2">
         <div v-for="(item, index) in itemsStore.items">
-        <router-link :to="'/detail/' + item">
-            <div class="flex my-4">
-                <div class="h-24 w-24 border-black border-2 flex items-center justify-center">
-                    <svg class="w-9 h-9" fill="none" xmlns="http://www.w3.org/2000/svg"></svg>
-                </div>
-                <div class="py-2 px-4">
-                    <h2 class="text-base pb-1">{{ item }}</h2>
-                    <div class="text-sm text-gray-dark">Miloš Alexander Bazovský  a ...</div>
-                </div>
-            </div>
-        </router-link>
+            <ThumbRow :item-id="item"></ThumbRow>
         </div>
 
         <router-link to="/">
@@ -48,6 +38,7 @@
     import { useItemsStore } from '../stores/ItemsStore'
     import ConfirmButton from '../components/ConfirmButton.vue'
     import Header from '../components/Header.vue'
+    import ThumbRow from '../components/ThumbRow.vue'
 
     const itemsStore = useItemsStore()
 
