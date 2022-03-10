f8dbd14ee36ca5eecf24062645db2fd33305037c
Igor Rjabinin <igor.rjabinin@gmail.com>
2022-03-10 10:50:20 +0100

    add thumb row to add new artwork in collection

--- resources/js/views/Collection.vue
+++ resources/js/views/Collection.vue
@@ -2,7 +2,21 @@
     <Header :code="1">{{ $t('My collection') }}</Header>
 
     <div class="h-full border-black p-4 border-t-2">
-        diela
+        <router-link to="/">
+            <div class="flex my-4">
+                <div class="h-24 w-24 border-black border-2 flex items-center justify-center">
+                    <svg class="w-9 h-9" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5 16h22M16 5v22" stroke="#2F3152" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>
+                </div>
+                <div class="py-2 px-4">
+                    <h2 class="text-base pb-1">{{ $t('Add new artwork') }}</h2>
+                    <div class="text-sm text-gray-dark">{{ $t('Tap and enter a new artwork code') }}</div>
+                </div>
+            </div>
+        </router-link>
+        <hr class="h-0.5 bg-gray-soft border-0 mt-6 mb-2">
+        <button class="py-4 w-full active:text-gray-dark">
+            {{ $t('Clear collection') }}
+        </button>
     </div>
     <div class="w-full h-24 fixed bottom-0">
         <div class="p-4">
