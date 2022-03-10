457373d76ef1ffe0761acb8119946cf1b08a0685
Igor Rjabinin <igor.rjabinin@gmail.com>
2022-03-10 11:07:12 +0100

    show basic list of item ids from store in collection

--- resources/js/views/Collection.vue
+++ resources/js/views/Collection.vue
@@ -2,6 +2,20 @@
     <Header :code="1">{{ $t('My collection') }}</Header>
 
     <div class="h-full border-black p-4 border-t-2">
+        <div v-for="(item, index) in itemsStore.items">
+        <router-link :to="'/detail/' + item">
+            <div class="flex my-4">
+                <div class="h-24 w-24 border-black border-2 flex items-center justify-center">
+                    <svg class="w-9 h-9" fill="none" xmlns="http://www.w3.org/2000/svg"></svg>
+                </div>
+                <div class="py-2 px-4">
+                    <h2 class="text-base pb-1">{{ item }}</h2>
+                    <div class="text-sm text-gray-dark">Miloš Alexander Bazovský  a ...</div>
+                </div>
+            </div>
+        </router-link>
+        </div>
+
         <router-link to="/">
             <div class="flex my-4">
                 <div class="h-24 w-24 border-black border-2 flex items-center justify-center">
@@ -27,7 +41,7 @@
     </div>
 </template>
 
-<script>
+<script setup>
     import {ref, onMounted} from 'vue'
     import { useRoute } from 'vue-router'
     import { getActiveLanguage } from 'laravel-vue-i18n';
@@ -35,8 +49,6 @@
     import ConfirmButton from '../components/ConfirmButton.vue'
     import Header from '../components/Header.vue'
 
-    export default {
-        components: { ConfirmButton, Header },
-        setup() { }
-    }
+    const itemsStore = useItemsStore()
+
 </script>
