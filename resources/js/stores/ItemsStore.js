ecfde0643cd9344cb53497dc4dfd49916e8c0565
Igor Rjabinin <igor.rjabinin@gmail.com>
2022-03-10 01:46:38 +0100

    use local storage for itemsStore

--- resources/js/stores/ItemsStore.js
+++ resources/js/stores/ItemsStore.js
@@ -1,8 +1,9 @@
 import { defineStore } from 'pinia';
+import { useStorage } from '@vueuse/core'
 
 export const useItemsStore = defineStore('ItemsStore', {
     state: () => ({
-        items: [],
+        items: useStorage('items', [])
     }),
     getters: {
         itemsCount() {
