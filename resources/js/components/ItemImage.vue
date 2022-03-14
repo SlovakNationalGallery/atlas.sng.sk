c5e68b723de14fe5e8b795932270c721ef940b6b
Igor Rjabinin <igor.rjabinin@gmail.com>
2022-03-14 23:14:35 +0100

    add component ItemImage + add srcset to item resource API

---
+++ resources/js/components/ItemImage.vue
@@ -0,0 +1,14 @@
+<template>
+    <img
+        class="h-full object-cover w-full"
+        :alt="`${ item.author }: ${ item.title }`"
+        :src="item.image_src"
+        :srcset="item.image_srcset"
+        sizes="1px"
+        onload="window.requestAnimationFrame(function(){if(!(size=getBoundingClientRect().width))return;onload=null;sizes=Math.ceil(size/window.innerWidth*100)+'vw';});"
+    />
+</template>
+
+<script setup>
+    const props = defineProps(['item']);
+</script>
