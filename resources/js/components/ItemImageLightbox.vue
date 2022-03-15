5f67dbf8dd0d2b09993e2ca859db755b3600c32f
Igor Rjabinin <igor.rjabinin@gmail.com>
2022-03-15 17:26:55 +0100

    NSNG-30 lightbox fix click on expand icon in detail

--- resources/js/components/ItemImageLightbox.vue
+++ resources/js/components/ItemImageLightbox.vue
@@ -1,5 +1,31 @@
 <template>
-    <ItemImage :item="item" @click.prevent="visible = true"></ItemImage>
+    <a href="#" class="block h-full w-full relative" @click.prevent="visible = true">
+        <ItemImage :item="item"></ItemImage>
+        <svg class="absolute bottom-3 left-3 w-8" fill="none" viewBox="0 0 32 35" xmlns="http://www.w3.org/2000/svg">
+            <g filter="url(#a)" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+            <path d="m21 6h5v5"/>
+            <path d="m19 13 7-7"/>
+            <path d="M11 26H6V21"/>
+            <path d="m13 19-7 7"/>
+            <path d="m26 21v5h-5"/>
+            <path d="m19 19 7 7"/>
+            <path d="m6 11v-5h5"/>
+            <path d="m13 13-7-7"/>
+            </g>
+            <defs>
+            <filter id="a" x="-4" y="0" width="40" height="40" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse">
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feColorMatrix in="SourceAlpha" result="hardAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+            <feOffset dy="4"/>
+            <feGaussianBlur stdDeviation="2"/>
+            <feComposite in2="hardAlpha" operator="out"/>
+            <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+            <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_2433_18536"/>
+            <feBlend in="SourceGraphic" in2="effect1_dropShadow_2433_18536" result="shape"/>
+            </filter>
+            </defs>
+        </svg>
+    </a>
     <div
         class="fixed inset-0 z-50 flex justify-center items-center"
         v-if="visible"
--- resources/js/views/Detail.vue
+++ resources/js/views/Detail.vue
@@ -1,32 +1,7 @@
 <template>
     <Header :code="(item!==null) ? item.code : '000000000'">{{ $t('Artwork detail') }}</Header>
-    <div class="bg-gray-softest h-48 border-black border-t-2 border-b-2 relative">
+    <div class="bg-gray-softest h-48 border-black border-t-2 border-b-2">
         <ItemImageLightbox v-if="item" :item="item"></ItemImageLightbox>
-        <svg class="absolute bottom-3 left-3 w-8" fill="none" viewBox="0 0 32 35" xmlns="http://www.w3.org/2000/svg">
-            <g filter="url(#a)" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-            <path d="m21 6h5v5"/>
-            <path d="m19 13 7-7"/>
-            <path d="M11 26H6V21"/>
-            <path d="m13 19-7 7"/>
-            <path d="m26 21v5h-5"/>
-            <path d="m19 19 7 7"/>
-            <path d="m6 11v-5h5"/>
-            <path d="m13 13-7-7"/>
-            </g>
-            <defs>
-            <filter id="a" x="-4" y="0" width="40" height="40" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse">
-            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-            <feColorMatrix in="SourceAlpha" result="hardAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
-            <feOffset dy="4"/>
-            <feGaussianBlur stdDeviation="2"/>
-            <feComposite in2="hardAlpha" operator="out"/>
-            <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
-            <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_2433_18536"/>
-            <feBlend in="SourceGraphic" in2="effect1_dropShadow_2433_18536" result="shape"/>
-            </filter>
-            </defs>
-        </svg>
-
     </div>
     <div class="h-full border-black p-4 pb-24" v-if="item">
         <h2 class="text-xl font-bold">{{ item.title }}</h2>
