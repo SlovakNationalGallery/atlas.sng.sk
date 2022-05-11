c36b7fe047199c7c0523d60d115472645ec5623b
Igor Rjabinin <igor.rjabinin@gmail.com>
2022-05-11 22:21:51 +0200

    add OnboardingStep component + use it to show all onboarding steps

--- resources/js/components/OnboardingModal.vue
+++ resources/js/components/OnboardingModal.vue
@@ -7,21 +7,44 @@
             class="bg-black opacity-70 absolute inset-0 cursor-zoom-out"
             @click="emit('close')"
         />
-        <div class="relative mx-4 bg-white w-full max-w-2xl bg-white rounded-xl px-4 py-6">
+
+        <OnboardingStep v-if="step == 0">
             <h3 class="font-bold text-2xl my-4">{{ $t('Collect art') }}</h3>
             <div class="text-base mb-4">{{ $t('Save interesting artworks from the exhibition to your mobile device.') }}</div>
             <div class="text-base">{{ $t('Insert the code located by the artwork into the grid. Press save in the artwork profile to collect it.') }}</div>
-            <ConfirmButton class="bg-black text-white my-4">{{ $t('Next') }}</ConfirmButton>
-            <!-- <ConfirmButton class="bg-black text-white my-4" @click="emit('close')">{{ $t('Close') }}</ConfirmButton> -->
-        </div>
+            <ConfirmButton class="bg-black text-white my-4" @click="next">{{ $t('Next') }}</ConfirmButton>
+        </OnboardingStep>
+
+        <OnboardingStep v-if="step == 1">
+            <h3 class="font-bold text-2xl my-4">{{ $t('Create a collection') }}</h3>
+            <div class="text-base mb-4">{{ $t('Saved artworks are added into your collection. Return to them later to find out more.') }}</div>
+            <div class="text-base">{{ $t('The collection will persist even after you close the application.') }}</div>
+            <ConfirmButton class="bg-black text-white my-4" @click="next">{{ $t('Next') }}</ConfirmButton>
+        </OnboardingStep>
+
+        <OnboardingStep v-if="step == 2">
+            <h3 class="font-bold text-2xl my-4">{{ $t('Share the collection') }}</h3>
+            <div class="text-base mb-4">{{ $t('Tap the Share button in your Collection, and send the link to your e-mail or share it with friends.') }}</div>
+            <div class="text-base">{{ $t('The shared link never expires.') }}</div>
+            <ConfirmButton class="bg-black text-white my-4" @click="emit('close')">{{ $t('Start collecting') }}</ConfirmButton>
+        </OnboardingStep>
     </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
 import ConfirmButton from '../components/ConfirmButton.vue'
+import OnboardingStep from '../components/OnboardingStep.vue'
+
 const props = defineProps({
     'visible': Boolean
-});
-const emit = defineEmits(['close']);
+})
+
+const step = ref(0)
+
+const emit = defineEmits(['close'])
+
+const next = () => {
+    step.value++
+}
 </script>
---
+++ resources/js/components/OnboardingStep.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="relative mx-4 bg-white w-full max-w-2xl rounded-xl px-4 py-6">
+        <slot />
+    </div>
+</template>
