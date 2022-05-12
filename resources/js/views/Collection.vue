22e82bf035e17bafe00cda146347f33a6263ae8f
Igor Rjabinin <igor.rjabinin@gmail.com>
2022-05-12 14:10:01 +0200

    set feedback URL per activeLanguage

--- resources/js/views/Collection.vue
+++ resources/js/views/Collection.vue
@@ -2,12 +2,15 @@
     <Header :code="'111111111'">{{ $t('My collection') }}</Header>
 
     <div class="border-black p-4 border-t-2">
-        <h3 class="text-lg font-bold mb-3">{{ $t('Help us make the app better') }}</h3>
+        <h3 class="text-lg font-bold mb-3">
+            {{ $t('Help us make the app better') }}
+        </h3>
         <div class="mb-4">
-            {{ $t('How did you like your experience with the app? Did it match your expectations?') }} <br>
+            {{ $t('How did you like your experience with the app? Did it match your expectations?') }}
+            <br />
             {{ $t('Let us know in the brief online survey.') }}
         </div>
-        <ConfirmButton @click="getFeedback" class="bg-black text-white" >
+        <ConfirmButton @click="getFeedback" class="bg-black text-white">
             {{ $t('Give feedback') }}
         </ConfirmButton>
     </div>
@@ -19,11 +22,21 @@
         <router-link to="/">
             <div class="flex my-4">
                 <div class="h-24 w-24 border-black border-2 flex items-center justify-center">
-                    <svg class="w-9 h-9" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5 16h22M16 5v22" stroke="#2F3152" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>
+                    <svg class="w-9 h-9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M5 16h22M16 5v22"
+                            stroke="#2F3152"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        />
+                    </svg>
                 </div>
                 <div class="py-2 px-4">
                     <h2 class="text-base pb-1">{{ $t('Add new artwork') }}</h2>
-                    <div class="text-sm text-gray-dark">{{ $t('Enter a new artwork code') }}</div>
+                    <div class="text-sm text-gray-dark">
+                        {{ $t('Enter a new artwork code') }}
+                    </div>
                 </div>
             </div>
         </router-link>
@@ -38,23 +51,70 @@
     <div class="mt-auto w-full md:max-w-lg bg-green border-black border-t-2">
         <div class="p-4">
             <div class="flex flex-col">
-                <h3 class="text-base font-bold mb-4" v-html="$t('Your collection will stay here even after you close the app. Get back to it later.')"></h3>
+                <h3
+                    class="text-base font-bold mb-4"
+                    v-html="$t('Your collection will stay here even after you close the app. Get back to it later.')"
+                ></h3>
                 <ul class="list-disc list-outside mb-4 ml-4">
-                    <li>{{ $t('Send the link to your e-mail or share it with friends.') }}</li>
+                    <li>
+                        {{ $t('Send the link to your e-mail or share it with friends.') }}
+                    </li>
                     <li>{{ $t('The link never expires.') }}</li>
                 </ul>
-                <ConfirmButton v-if="!shareUrl"  class="bg-black text-white" @click="shareCollection" :disabled="loading">
+                <ConfirmButton
+                    v-if="!shareUrl"
+                    class="bg-black text-white"
+                    @click="shareCollection"
+                    :disabled="loading"
+                >
                     <span v-if="loading">{{ $t('Loading...') }}</span>
                     <span v-else>{{ $t('Share collection link') }}</span>
                 </ConfirmButton>
                 <ConfirmButton v-else class="bg-black text-white" @click="shareUrlDialog(true)">
                     <div class="flex justify-center items-center">
-                        <svg class="mr-2" width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M6.5 15C8.15685 15 9.5 13.6569 9.5 12C9.5 10.3431 8.15685 9 6.5 9C4.84315 9 3.5 10.3431 3.5 12C3.5 13.6569 4.84315 15 6.5 15Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                            <path d="M17 21.75C18.6569 21.75 20 20.4069 20 18.75C20 17.0931 18.6569 15.75 17 15.75C15.3431 15.75 14 17.0931 14 18.75C14 20.4069 15.3431 21.75 17 21.75Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                            <path d="M17 8.25C18.6569 8.25 20 6.90685 20 5.25C20 3.59315 18.6569 2.25 17 2.25C15.3431 2.25 14 3.59315 14 5.25C14 6.90685 15.3431 8.25 17 8.25Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                            <path d="M14.4782 6.87189L9.02197 10.3781" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                            <path d="M9.02197 13.6219L14.4782 17.1281" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <svg
+                            class="mr-2"
+                            width="25"
+                            height="24"
+                            viewBox="0 0 25 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M6.5 15C8.15685 15 9.5 13.6569 9.5 12C9.5 10.3431 8.15685 9 6.5 9C4.84315 9 3.5 10.3431 3.5 12C3.5 13.6569 4.84315 15 6.5 15Z"
+                                stroke="white"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            />
+                            <path
+                                d="M17 21.75C18.6569 21.75 20 20.4069 20 18.75C20 17.0931 18.6569 15.75 17 15.75C15.3431 15.75 14 17.0931 14 18.75C14 20.4069 15.3431 21.75 17 21.75Z"
+                                stroke="white"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            />
+                            <path
+                                d="M17 8.25C18.6569 8.25 20 6.90685 20 5.25C20 3.59315 18.6569 2.25 17 2.25C15.3431 2.25 14 3.59315 14 5.25C14 6.90685 15.3431 8.25 17 8.25Z"
+                                stroke="white"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            />
+                            <path
+                                d="M14.4782 6.87189L9.02197 10.3781"
+                                stroke="white"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            />
+                            <path
+                                d="M9.02197 13.6219L14.4782 17.1281"
+                                stroke="white"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            />
                         </svg>
                         <div>{{ shareUrl }}</div>
                     </div>
@@ -66,13 +126,14 @@
 </template>
 
 <script setup>
-import {ref, onMounted} from 'vue'
+import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import { useItemsStore } from '../stores/ItemsStore'
 import ConfirmButton from '../components/ConfirmButton.vue'
 import Header from '../components/Header.vue'
 import ThumbRow from '../components/ThumbRow.vue'
 import CardModal from '../components/CardModal.vue'
+import { getActiveLanguage } from 'laravel-vue-i18n'
 
 const route = useRoute()
 const itemsStore = useItemsStore()
@@ -80,23 +141,25 @@ const modalActive = ref(false)
 const loading = ref(false)
 const shareUrl = ref('')
 const toggleModal = () => {
-    modalActive.value = !modalActive.value;
-};
+    modalActive.value = !modalActive.value
+}
 
 const shareCollection = () => {
     loading.value = true
-    axios.post('/api/collections', {
-        'items': itemsStore.items
-      })
-      .then((res) => {
-        shareUrl.value = res.data.url
-        shareUrlDialog()
-      })
-      .catch((err) => {
-        console.log(err)
-      }).finally(() => {
-        loading.value = false
-      })
+    axios
+        .post('/api/collections', {
+            items: itemsStore.items,
+        })
+        .then((res) => {
+            shareUrl.value = res.data.url
+            shareUrlDialog()
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+        .finally(() => {
+            loading.value = false
+        })
 }
 
 const shareUrlDialog = (openLink = false) => {
@@ -112,7 +175,11 @@ const shareUrlDialog = (openLink = false) => {
 }
 
 const getFeedback = () => {
-    window.open('https://602v7jq58zi.typeform.com/to/OGgCJT8e', '_blank').focus()
+    if (getActiveLanguage() == 'sk') {
+        window.open('https://602v7jq58zi.typeform.com/to/OGgCJT8e', '_blank').focus()
+    } else {
+        window.open('https://602v7jq58zi.typeform.com/to/VKGfiUpz', '_blank').focus()
+    }
 }
 
 onMounted(async () => {
@@ -120,5 +187,4 @@ onMounted(async () => {
         itemsStore.fetch(route.params.id)
     }
 })
-
 </script>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,37 +1,53 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="md:bg-gray-softest">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width,initial-scale=1.0">
-        
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-        <link rel="manifest" href="/site.webmanifest">
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000">
-        <meta name="theme-color" content="#ffffff">
 
-        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-        <title>{{ trans('Codes SNG') }}</title>
-        @if (App::environment('production'))
-            {{-- Clarity --}}
-            <script type="text/javascript"> (function(c,l,a,r,i,t,y){ c[a]=c[a]||function()
-            {(c[a].q=c[a].q||[]).push(arguments)}
-            ; t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i; y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y); })(window, document, "clarity", "script", "az0i6tv0rb"); </script>
-             {{-- /Clarity --}}
-             {{-- GA --}}
-             <script async src="https://www.googletagmanager.com/gtag/js?id=UA-222618680-1"></script>
-             <script>
-               window.dataLayer = window.dataLayer || [];
-               function gtag(){dataLayer.push(arguments);}
-               gtag('js', new Date());  gtag('config', 'UA-222618680-1');
-             </script>
-             {{-- /GA --}}
-        @endif
-    </head>
-    <body>
-        <div id="app" class="md:max-w-lg md:mx-auto md:my-auto bg-white min-h-screen flex flex-col"></div>
-        <script src="{{ mix('/js/app.js') }}"></script>
-    </body>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000">
+    <meta name="theme-color" content="#ffffff">
+
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <title>{{ trans('Codes SNG') }}</title>
+    @if (App::environment('production'))
+        {{-- Clarity --}}
+        <script type="text/javascript">
+            (function(c, l, a, r, i, t, y) {
+                c[a] = c[a] || function() {
+                    (c[a].q = c[a].q || []).push(arguments)
+                };
+                t = l.createElement(r);
+                t.async = 1;
+                t.src = "https://www.clarity.ms/tag/" + i;
+                y = l.getElementsByTagName(r)[0];
+                y.parentNode.insertBefore(t, y);
+            })(window, document, "clarity", "script", "az0i6tv0rb");
+        </script>
+        {{-- /Clarity --}}
+        {{-- GA --}}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-222618680-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+            gtag('config', 'UA-222618680-1');
+        </script>
+        {{-- /GA --}}
+    @endif
+</head>
+
+<body>
+    <div id="app" class="md:max-w-lg md:mx-auto md:my-auto bg-white min-h-screen flex flex-col"></div>
+    <script src="{{ mix('/js/app.js') }}"></script>
+</body>
+
 </html>
