cfd5b760352bcc056c8d2bad70c161928d939d99
Igor Rjabinin <igor.rjabinin@gmail.com>
2022-05-24 22:58:15 +0200

    detect browser locale NSNG-54

--- resources/js/app.js
+++ resources/js/app.js
@@ -22,11 +22,26 @@ const router = createRouter({
     routes,
 })
 
+const getBrowserLocale = () => {
+  const navigatorLocale =
+    navigator.languages !== undefined
+      ? navigator.languages[0]
+      : navigator.language
+
+  if (!navigatorLocale) {
+    return undefined
+  }
+
+  const trimmedLocale = navigatorLocale.trim().split(/-|_/)[0]
+  return trimmedLocale
+}
+
+
 const app = createApp(App)
 app.use(router)
 app.use(i18nVue, {
+    lang: getBrowserLocale(),
     resolve: lang => import(`../lang/${lang}.json`),
 })
 app.use(createPinia())
 app.mount('#app')
-
