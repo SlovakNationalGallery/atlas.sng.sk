430bee38d6e8340482ece533ff45257b62d890eb
Igor Rjabinin <igor.rjabinin@gmail.com>
2022-08-18 15:33:49 +0200

    make itemImage movable

--- resources/js/components/ItemImage.vue
+++ resources/js/components/ItemImage.vue
@@ -1,14 +1,42 @@
 <template>
     <img
-        class="h-full object-cover w-full"
+        class="h-full object-cover w-full object-top"
+        :style="{ objectPosition: 'center ' + offsetTop + 'px' }"
         :alt="`${ item.author }: ${ item.title }`"
         :src="item.image_src"
         :srcset="item.image_srcset"
         sizes="1px"
         onload="window.requestAnimationFrame(function(){if(!(size=getBoundingClientRect().width))return;onload=null;sizes=Math.ceil(size/window.innerWidth*100)+'vw';});"
+        @mousemove.stop.prevent="move"
+        @mousedown.stop.prevent="moveStart"
+        draggable="false"
     />
 </template>
 
 <script setup>
+    import { ref } from "vue";
+
     const props = defineProps(['item']);
+    const isMoving = ref(false);``
+    const startPosY = ref(0);
+    const offsetTop = ref(0);
+
+    const move = (event) => {
+        if (isMoving.value) {
+            offsetTop.value = event.pageY - startPosY.value
+            console.log('offsetTop: ' + offsetTop.value)
+        }
+    }
+
+    const moveStart = (event) => {
+        isMoving.value = true
+        startPosY.value = event.pageY
+    }
+
+    const moveEnd = (event) => {
+        isMoving.value = false
+    }
+
+    window.addEventListener('mouseup', moveEnd)
+
 </script>
