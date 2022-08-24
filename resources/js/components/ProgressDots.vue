01812c822372029f5b51f503ec0b42005a0e76fb
FrantisekMichalSebestyen <sebestyen.frantisek.michal@gmail.com>
2022-08-24 10:40:03 +0200

    feat: add onboarding transitions

---
+++ resources/js/components/ProgressDots.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex w-full content-center justify-center">
+        <span
+            v-for="n in length"
+            class="h-2 w-2 bg-white mx-1 rounded-full border-2"
+            :class="{ 'bg-black': activeStep === n }"
+        >
+        </span>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps(['length', 'activeStep'])
+</script>
