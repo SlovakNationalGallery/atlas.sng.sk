5f11a1609f9bb6990b4ea3aadb230a6c88413b2c
FrantisekMichalSebestyen <sebestyen.frantisek.michal@gmail.com>
2022-09-06 10:42:10 +0200

    feat: add onboarding transitions (#28)
    * feat: add onboarding transitions
    
    * fix duplicated translation
    
    * fix translation string
    
    * fix align and spacing
    
    * fix: overflow-hidden on desktop
    
    Co-authored-by: Igor Rjabinin <igor.rjabinin@gmail.com>

--- resources/js/components/OnboardingModal.vue
+++ resources/js/components/OnboardingModal.vue
@@ -1,83 +1,57 @@
 <template>
-    <div class="fixed inset-0 z-50 flex justify-center items-center" v-if="visible">
-        <div
-            class="bg-black opacity-70 absolute inset-0 cursor-zoom-out"
-            @click="
-                reset(),
-                emit('close')
-            "
-        />
-
-        <OnboardingStep v-if="step == 0">
-            <h3 class="font-bold text-2xl my-4">{{ $t('Collect art') }}</h3>
-            <div
-                class="text-base mb-4"
-                v-html="$t('Save interesting artworks from the exhibition to your mobile device.')"
-            ></div>
-            <div
-                class="text-base"
-                v-html="
-                    $t(
-                        'Insert the code located by the artwork into the grid. Press save in the artwork profile to collect it.'
-                    )
-                "
-            ></div>
-            <ConfirmButton class="bg-black text-white my-4" @click="next">{{ $t('Next') }}</ConfirmButton>
-        </OnboardingStep>
-
-        <OnboardingStep v-if="step == 1">
-            <h3 class="font-bold text-2xl my-4">{{ $t('Create a collection') }}</h3>
-            <div
-                class="text-base mb-4"
-                v-html="$t('Saved artworks are added into your collection. Return to them later to find out more.')"
-            ></div>
-            <div
-                class="text-base"
-                v-html="$t('The collection will persist even after you close the application.')"
-            ></div>
-            <ConfirmButton class="bg-black text-white my-4" @click="next">{{ $t('Next') }}</ConfirmButton>
-        </OnboardingStep>
-
-        <OnboardingStep v-if="step == 2">
-            <h3 class="font-bold text-2xl my-4">{{ $t('Share the collection') }}</h3>
-            <div
-                class="text-base mb-4"
-                v-html="
-                    $t(
-                        'Tap the Share button in your collection, and send the link to your e-mail or share it with friends.'
-                    )
-                "
-            ></div>
-            <div class="text-base font-bold">{{ $t('The shared link never expires.') }}</div>
-            <ConfirmButton
-                class="bg-black text-white my-4"
-                @click="
-                    reset(),
-                    emit('close')
-                "
-                >{{ $t('Start collecting') }}</ConfirmButton
-            >
-        </OnboardingStep>
+    <div
+        class="md:max-w-lg md:mx-auto fixed inset-0 z-50 flex justify-center items-center overflow-hidden"
+        v-if="visible"
+    >
+        <div class="bg-black opacity-70 absolute inset-0 cursor-zoom-out" @click="close" />
+        <Carousel :items-to-show="2.1" :wrap-around="true" class="min-w-[calc(190%)]" ref="myCarousel">
+            <Slide :key="0">
+                <OnboardingStep
+                    :btnFn="next"
+                    :title="'Collect art'"
+                    :body1="'Save interesting artworks from the exhibition to your mobile device.'"
+                    :body2="'Insert the code located by the artwork into the grid. Press save in the artwork profile to collect it.'"
+                    :btnText="'Next'"
+                    :active-step="1"
+                />
+            </Slide>
+            <Slide :key="1">
+                <OnboardingStep
+                    :btnFn="next"
+                    :title="'Create a collection'"
+                    :body1="'Saved artworks are added into your collection. Return to them later to find out more.'"
+                    :body2="'The collection will persist even after you close the application.'"
+                    :btnText="'Next'"
+                    :active-step="2"
+                />
+            </Slide>
+            <Slide :key="3">
+                <OnboardingStep
+                    :btnFn="close"
+                    :title="'Share the collection'"
+                    :body1="'Tap the Share button in your collection, and send the link to your e-mail or share it with friends.'"
+                    :body2="'The shared link never expires.'"
+                    :btnText="'Start collecting'"
+                    :active-step="3"
+                />
+            </Slide>
+        </Carousel>
     </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-import ConfirmButton from '../components/ConfirmButton.vue'
+import { Carousel, Slide } from 'vue3-carousel'
+import 'vue3-carousel/dist/carousel.css'
 import OnboardingStep from '../components/OnboardingStep.vue'
 
 const props = defineProps({
     visible: Boolean,
 })
 
-const step = ref(0)
-
+const myCarousel = ref(null)
 const emit = defineEmits(['close'])
 
-const next = () => {
-    step.value++
-}
-const reset = () => {
-    step.value = 0
-}
+const next = () => myCarousel.value?.next()
+const close = () => emit('close')
 </script>
--- resources/js/components/OnboardingStep.vue
+++ resources/js/components/OnboardingStep.vue
@@ -1,5 +1,20 @@
 <template>
-    <div class="relative mx-4 bg-white w-full max-w-2xl rounded-xl px-4 py-6">
-        <slot />
+    <div class="flex-col flex mx-1 bg-white h-full max-w-2xl rounded-xl px-4 py-6">
+        <div class="flex-1 text-left">
+            <h3 class="font-bold text-2xl my-4">{{ $t(props.title) }}</h3>
+            <div class="text-base mb-4" v-html="$t(props.body1)" />
+            <div class="text-base" v-html="$t(props.body2)" />
+        </div>
+        <ConfirmButton class="flex-none bottom-0 bg-black text-white mt-6 mb-4" @click="btnFn">{{
+            $t(btnText)
+        }}</ConfirmButton>
+        <ProgressDots :length="3" :active-step="activeStep"></ProgressDots>
     </div>
 </template>
+
+<script setup>
+import ConfirmButton from '../components/ConfirmButton.vue'
+import ProgressDots from './ProgressDots.vue'
+
+const props = defineProps(['title', 'body1', 'body2', 'btnFn', 'btnText', 'activeStep'])
+</script>
