629f5916c9e17f8ac527741ef922d1b2cc8b9286
memb <ustengrav@users.noreply.github.com>
2022-10-04 16:39:54 +0200

    update english strings in TeaserModal.vue

--- resources/js/components/TeaserModal.vue
+++ resources/js/components/TeaserModal.vue
@@ -4,11 +4,11 @@
         <div class="relative mx-4 bg-white w-full max-w-2xl bg-white rounded-xl">
             <img src="/images/dusan-vyklad.jpg" alt="Výklad kurátora Dušana Burana" class="w-full rounded-t-xl" />
             <div class="px-4 py-6">
-                <h3 class="font-bold text-lg mb-2">{{ $t('What does Július Koller see?') }}</h3>
+                <h3 class="font-bold text-lg mb-2">{{ $t('Curator's insight in your pocket') }}</h3>
                 <div class="">
-                    {{ $t('Invasion of Czechoslovakia inspired the artist to create an UFO-themed alter-ego.') }}
+                    {{ $t('How does the Gothic altar work? Why are some statues missing their hands? What is the purpose of gilding?') }}
                 </div>
-                <div class="mt-4" v-html="$t('Insert the <b>code by the artwork</b> and find out more.')"></div>
+                <div class="mt-4" v-html="$t('Find simple <b>codes in the exhibition</b> and discover the secrets of medieval artistic workshops.')"></div>
                 <ConfirmButton class="bg-black text-white my-4" @click="emit('close')">{{ $t('OK') }}</ConfirmButton>
             </div>
         </div>
