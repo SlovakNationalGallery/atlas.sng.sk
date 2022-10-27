ad3ac032e2b7aaba1c8476f58b1c754fe81560b2
Igor Rjabinin <igor.rjabinin@gmail.com>
2022-10-27 23:15:02 +0200

    style button component

--- resources/js/components/ConfirmButton.vue
+++ resources/js/components/ConfirmButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="border-black border-2 py-4 w-full font-bold active:bg-black active:text-white">
+    <button class="border-black rounded-xl border-2 py-4 w-full font-bold active:bg-black active:text-white flex items-center justify-center">
         <slot></slot>
     </button>
 </template>
