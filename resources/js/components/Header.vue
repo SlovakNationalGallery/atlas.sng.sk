53895421450ea77a54d2873176b88e3fcd51808d
Igor Rjabinin <igor.rjabinin@gmail.com>
2022-11-03 15:16:47 +0100

    refactor inline svg-s

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -2,13 +2,11 @@
     <div class="border-black border-t-2 flex items-center">
         <router-link to="/">
             <div class="p-2.5 border-r-2" :class="[code ? 'border-r-transparent' : 'bg-green border-r-black']">
-                <svg v-if="code" class="w-[10px] h-[18px] ml-2" viewBox="0 0 10 18" fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
+                <svg v-if="code" class="w-[10px] h-[18px] ml-2 fill-none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M9 16.5L1.5 9L9 1.5"
                         class="stroke-2 stroke-black [stroke-linecap:round] [stroke-linejoin:round]" />
                 </svg>
-                <svg v-else class="h-6 w-6 fill-black" viewBox="0 0 32 32" fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
+                <svg v-else class="h-[26px] w-[26px] fill-black" viewBox="0 0 32 32">
                     <path fill-rule="evenodd" clip-rule="evenodd"
                         d="M0 8.54668V17.0934H7.45332H14.9066V23.4533V29.8133H8.54677H2.1869L2.17911 24.5544L2.17132 19.2955L1.08566 19.2874L0 19.2793V25.6396V32H16H32V16V0H16H0V8.54668ZM14.9066 8.54668V14.9066H8.54668H2.18672V8.54668V2.18672H8.54668H14.9066V8.54668ZM29.8057 7.76901C29.81 10.8308 29.803 13.3359 29.7902 13.3359C29.7775 13.3359 27.2585 10.8273 24.1925 7.76125L18.6179 2.18659L24.2079 2.19436L29.7979 2.20212L29.8057 7.76901ZM22.6833 9.33205L28.2579 14.9066H22.6602H17.0626V9.33205C17.0626 6.26603 17.073 3.75746 17.0857 3.75746C17.0984 3.75746 19.6174 6.26603 22.6833 9.33205ZM29.8133 19.7267V22.36H24.5467H19.2801V23.4532V24.5465L24.539 24.5543L29.7979 24.5621L29.8058 27.1877L29.8136 29.8133H23.4381H17.0626V23.4739C17.0626 19.9872 17.0718 17.1252 17.0831 17.1139C17.0944 17.1026 19.9633 17.0934 23.4585 17.0934H29.8133V19.7267Z" />
                 </svg>
