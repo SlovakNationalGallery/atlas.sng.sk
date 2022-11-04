51e14a6feb95d9131f0e4f5ea8515b3d61a3af44
Igor Rjabinin <igor.rjabinin@gmail.com>
2022-11-04 17:45:23 +0100

    update button size according the current design

--- resources/js/components/ConfirmButton.vue
+++ resources/js/components/ConfirmButton.vue
@@ -1,5 +1,6 @@
 <template>
-    <button class="border-black rounded-xl border-2 py-4 w-full font-bold active:bg-black active:text-white flex items-center justify-center text-base">
+    <button
+        class="border-black rounded-xl border-2 py-3 w-full font-bold active:bg-black active:text-white flex items-center justify-center text-base">
         <slot></slot>
     </button>
 </template>
