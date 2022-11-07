2314c5979a30540e0b95dce2d025e744de82016d
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2022-11-07 17:01:12 +0100

    common layout

--- resources/views/sections.blade.php
+++ resources/views/sections.blade.php
@@ -1,23 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.master')
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0">
-
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000">
-    <meta name="theme-color" content="#ffffff">
-
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-    <title>{{ trans('Sections') }} | {{ trans('SNG Codes') }}</title>
-</head>
-
-<body>
+@section('content')
     <div class="mx-12 my-12">
         <h1 class="font-bold mb-8 pl-8 text-2xl">{{ trans('Sections') }}</h1>
 
@@ -40,7 +23,4 @@
             </tbody>
         </table>
     </div>
-    <script src="{{ mix('/js/app.js') }}"></script>
-</body>
-
-</html>
+@endsection
