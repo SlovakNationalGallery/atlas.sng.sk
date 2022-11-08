d1942e68fc6a05a145184761de60e8dec3c5fa87
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2022-11-08 22:29:07 +0100

    mix version

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,7 @@ const mix = require('laravel-mix')
 mix.js('resources/js/app.js', 'public/js')
     .vue()
     .postCss('resources/css/app.css', 'public/css', [require('tailwindcss')])
+
+if (mix.inProduction()) {
+    mix.version()
+}
