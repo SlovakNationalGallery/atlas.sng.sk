053629f4e691fb1686e19ee219bb100ec82e3f7d
Igor Rjabinin <igor.rjabinin@gmail.com>
2022-11-16 13:17:58 +0100

    import and show exhibition for sections

--- resources/views/sections.blade.php
+++ resources/views/sections.blade.php
@@ -14,6 +14,7 @@
                     <th class="border-b font-bold p-4 pl-8 pt-0 pb-3 text-left">#</th>
                     <th class="border-b font-bold p-4 pl-8 pt-0 pb-3 text-left">Kód</th>
                     <th class="border-b font-bold p-4 pl-8 pt-0 pb-3 text-left">Názov</th>
+                    <th class="border-b font-bold p-4 pl-8 pt-0 pb-3 text-left">Výstava</th>
                 </tr>
             </thead>
             <tbody class="bg-white">
@@ -22,6 +23,7 @@
                     <td class="border-b border-gray-soft p-4 pl-8">{{ $section->code->id }}</td>
                     <td class="border-b border-gray-soft p-4 pl-8"><img class="h-auto w-10" src="/img/{{ $section->code->code }}.svg?color=black&v=1" alt="{{ $section->code->code }}" /></td>
                     <td class="border-b border-gray-soft p-4 pl-8">{{ $section->title }}</td>
+                    <td class="border-b border-gray-soft p-4 pl-8">{{ $section->code->exhibition->name ?? '' }}</td>
                 </tr>
                 @endforeach
             </tbody>
