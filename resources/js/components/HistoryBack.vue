b7568d4fa8a014a36a3b89a678d0bf9bd0c074f4
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2022-11-29 22:07:20 +0100

    back button

---
+++ resources/js/components/HistoryBack.vue
@@ -0,0 +1,19 @@
+<template>
+    <slot :back="back"></slot>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+import { useHistoryStore } from '../stores/HistoryStore'
+
+const router = useRouter()
+const historyStore = useHistoryStore()
+
+const back = () => {
+    if (historyStore.getState().back) {
+        router.back()
+    } else {
+        router.push({ name: 'home' })
+    }
+}
+</script>
