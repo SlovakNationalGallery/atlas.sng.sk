a84c20c5635f092b3db7c8e9e17c8bc6a5ba7027
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2022-12-01 00:41:31 +0100

    stories history

---
+++ resources/js/stores/StoriesStore.js
@@ -0,0 +1,20 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useStoriesStore = defineStore('StoriesStore', {
+    state: () => ({
+        stories: ref([]),
+        selectedLinks: ref([]),
+        active: ref(null),
+    }),
+    actions: {
+        addStory(story) {
+            this.stories.push(story)
+            this.active = story
+        },
+        selectLink(link) {
+            this.selectedLinks.push(link)
+            this.active = null
+        },
+    },
+})
