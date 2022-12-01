67b4295487685ebfe97850922efeffc9d08dd816
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2022-12-01 01:22:15 +0100

    throw vimeo response error

--- app/Video/VimeoApi.php
+++ app/Video/VimeoApi.php
@@ -9,14 +9,18 @@ class VimeoApi
 {
     public function getThumbnail($videoId)
     {
+        $response = Http::vimeo()
+            ->get("/videos/$videoId/pictures")
+            ->object();
+
+        if (isset($response->error)) {
+            throw new \Exception($response->error);
+        }
+
         return Cache::remember(
             sprintf('vimeo_thumbnail.%s', $videoId),
             now()->addHour(),
-            fn() => collect(
-                Http::vimeo()
-                    ->get("/videos/$videoId/pictures")
-                    ->object()->data
-            )->first(fn($thumbnail) => $thumbnail->active)
+            fn() => collect($response->data)->first(fn($thumbnail) => $thumbnail->active)
         );
     }
 }
