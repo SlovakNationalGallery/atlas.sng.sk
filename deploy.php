9135fe1ed2b462710c5f9bf4603c9e68b4a5098a
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2022-12-01 03:04:38 +0100

    fix deploy

--- deploy.php
+++ deploy.php
@@ -2,7 +2,6 @@
 namespace Deployer;
 
 require 'recipe/laravel.php';
-require 'contrib/npm.php';
 
 set('bin/php', 'php8.1');
 set('bin/composer', '{{bin/php}} $(which composer)');
@@ -13,37 +12,25 @@ set('application', 'atlas');
 // Project repository
 set('repository', 'git@github.com:SlovakNationalGallery/atlas.sng.sk.git');
 
-// [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true);
-
 // Shared files/dirs between deploys
-add('shared_files', []);
-add('shared_dirs', ['storage', 'resources/fonts']);
+add('shared_dirs', ['resources/fonts']);
 
-// Writable dirs by web server
-add('writable_dirs', []);
 set('allow_anonymous_stats', false);
 
 // Hosts
-
 host('lab_sng@webumenia.sk')
     ->set('deploy_path', '/var/www/atlas.sng.sk')
     ->set('user', 'lab_sng');
 
 // Tasks
-
 task('build', function () {
     cd('{{release_path}}');
 
     run('source "$HOME/.nvm/nvm.sh" && nvm use 16.16.0 && npm ci && npm run build');
 });
 
-// [Optional] if deploy fails automatically unlock.
+// Hooks
+before('artisan:migrate', 'artisan:cache:clear');
+after('deploy:vendors', 'build');
+after('deploy:symlink', 'artisan:queue:restart');
 after('deploy:failed', 'deploy:unlock');
-
-// Migrate database before symlink new release.
-
-before('deploy:symlink', 'artisan:migrate');
-
-after('deploy:update_code', 'npm:install');
-after('deploy:shared', 'build');
