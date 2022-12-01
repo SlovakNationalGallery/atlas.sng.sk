0942bcc0b5b0ebf7d294fb2ce3dfa320c6b54b2c
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2022-12-01 03:38:32 +0100

    disable code panel peeking

--- resources/js/stores/StoriesStore.js
+++ resources/js/stores/StoriesStore.js
@@ -6,7 +6,7 @@ export const useStoriesStore = defineStore('StoriesStore', {
         stories: ref([]),
         selectedLinks: ref([]),
         active: ref(null),
-        peekCodePanel: ref(true),
+        peekCodePanel: ref(false),
     }),
     actions: {
         addStory(story) {
