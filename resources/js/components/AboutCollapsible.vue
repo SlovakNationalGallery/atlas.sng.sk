1f097d3f8b7d2f9fde0a326818aae61eab647acc
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2022-12-09 00:55:11 +0100

    about texts

--- resources/js/components/AboutCollapsible.vue
+++ resources/js/components/AboutCollapsible.vue
@@ -1,19 +1,32 @@
 <template>
-    <div>
-        <div @click="open = !open" class="cursor-pointer flex p-4 text-xl">
+    <div ref="el">
+        <div @click="open = !open" class="flex cursor-pointer p-4 text-xl">
             <div class="grow">
                 <slot name="summary"></slot>
             </div>
             <SvgDownArrow :class="{ 'rotate-180': open }" />
         </div>
-        <div class="px-4" v-show="open">
+        <div class="px-4 pb-4" v-show="open">
             <slot name="content"></slot>
         </div>
     </div>
 </template>
 
 <script setup>
+import { nextTick, ref, watch } from 'vue'
 import SvgDownArrow from './svg/DownArrow.vue'
 
-const props = defineProps(['open'])
+const props = defineProps(['initialOpen'])
+const el = ref(null)
+const open = ref(props.initialOpen)
+
+watch(open, (value) => {
+    if (value) {
+        nextTick(() => {
+            el.value.scrollIntoView({
+                behavior: 'smooth',
+            })
+        })
+    }
+})
 </script>
--- resources/views/import.blade.php
+++ resources/views/import.blade.php
@@ -1,11 +1,19 @@
 @extends('layouts.master')
 
+@section('title')
+    Import | @parent
+@endsection
+
 @section('content')
     <div class="mx-12 my-12">
-        <a class="underline hover:no-underline" target="_blank" href="{{ route('import_type', ['type' => 'sections']) }}">sections (w/ items, exhibitions, stories)</a><br />
-        <a class="underline hover:no-underline" target="_blank" href="{{ route('import_type', ['type' => 'items']) }}">items (w/ exhibitions, stories)</a><br />
-        <a class="underline hover:no-underline" target="_blank" href="{{ route('import_type', ['type' => 'exhibitions']) }}">exhibitions</a><br />
-        <a class="underline hover:no-underline" target="_blank" href="{{ route('import_type', ['type' => 'authorities']) }}">authorities</a><br />
-        <a class="underline hover:no-underline" target="_blank" href="{{ route('import_type', ['type' => 'stories']) }}">stories</a><br />
+        <h1 class="font-bold mb-8 pl-8 text-2xl">Import</h1>
+
+        <div class="pl-8">
+            <a class="underline hover:no-underline" target="_blank" href="{{ route('import_type', ['type' => 'sections']) }}">sections (w/ items, exhibitions, stories)</a><br />
+            <a class="underline hover:no-underline" target="_blank" href="{{ route('import_type', ['type' => 'items']) }}">items (w/ exhibitions, stories)</a><br />
+            <a class="underline hover:no-underline" target="_blank" href="{{ route('import_type', ['type' => 'exhibitions']) }}">exhibitions</a><br />
+            <a class="underline hover:no-underline" target="_blank" href="{{ route('import_type', ['type' => 'authorities']) }}">authorities</a><br />
+            <a class="underline hover:no-underline" target="_blank" href="{{ route('import_type', ['type' => 'stories']) }}">stories</a><br />
+        </div>
     </div>
 @endsection
