e46c2180406f752bb29e7a5c5e8891f30d9666da
Igor Rjabinin <igor.rjabinin@gmail.com>
2022-12-09 11:34:21 +0100

    fix sng logo in about + fix leading in about headline + border size for separators

--- resources/js/components/About.vue
+++ resources/js/components/About.vue
@@ -2,11 +2,11 @@
     <Transition :duration="300">
         <div class="fixed inset-0 z-10 overflow-hidden md:mx-auto md:max-w-lg" v-if="opened">
             <div class="h-full overflow-y-auto bg-white pt-[48px] duration-300">
-                <div class="border-b-2 border-gray-softest">
+                <div class="border-b-1 border-gray-softest">
                     <div class="my-8 flex items-end justify-between">
                         <div class="self-items-end mx-4">
-                            <SvgLogo class="mb-[14px] ml-[-5.25px] !h-[48px] !w-[48px]" viewBox="0 0 32 32" />
-                            <h1 class="font-bold leading-[1]">
+                            <SvgLogo class="mb-7 !h-[46px] !w-[46px]" viewBox="0 0 26 26" />
+                            <h1 class="font-bold leading-10">
                                 <span class="text-[52px]">Atlas</span><br />
                                 <span class="text-[42px]">SNG</span>
                             </h1>
@@ -16,79 +16,34 @@
                         </div>
                     </div>
 
-                    <div class="m-4 space-y-4 text-xl">
-                        <p v-html="$t('about_perex_1')"></p>
-                        <p v-html="$t('about_perex_2')"></p>
-                    </div>
+                    <p class="m-4 text-xl">
+                        Objavte príbehy umeleckých diel s digitálnou sprievodkyňou! Svetom novej SNG vás prevedie
+                        <span class="font-bold text-red-pastel">Ester</span> a jej atlas.sng.sk.
+                    </p>
                 </div>
 
-                <AboutCollapsible :initialOpen="true" class="scroll-mt-12 border-b-2 border-gray-softest">
+                <AboutCollapsible :open="true" class="border-b-1 border-gray-softest">
                     <template v-slot:summary>Ako na to?</template>
                     <template v-slot:content>
-                        <div class="space-y-4">
-                            <ul class="ml-6 list-disc">
-                                <li class="pl-2.5">Prihláste sa na SNG WIFI (heslo: welcometosng)</li>
-                                <li class="pl-2.5">
-                                    Vyberte si jednu z výstav a preskúmajte diela, na ktoré počas návštevy narazíte.
-                                </li>
-                                <li class="pl-2.5">Kód pri diele vyťukajte do mriežky v aplikácii.</li>
-                                <li class="pl-2.5">
-                                    Diela si môžete uložiť do svojej kolekcie a poslať na e-mail a vrátiť sa k nim doma
-                                    s rozšírenými informáciami z archívu.
-                                </li>
-                            </ul>
-                            <p>
-                                Ester vám pri putovaní výstavou ponúkne videá zo zákulisia a originálne pohľady na diela
-                                od vystavujúcich osobností, alebo návštevníkov a návštevníčok.
-                            </p>
-                        </div>
-                    </template>
-                </AboutCollapsible>
-                <AboutCollapsible class="scroll-mt-12 border-b-2 border-gray-softest">
-                    <template v-slot:summary>Projekt</template>
-                    <template v-slot:content>
-                        <div class="space-y-4">
-                            <p v-html="$t('about_project_1')"></p>
-                            <p v-html="$t('about_project_2')"></p>
-                            <p>
-                                <a
-                                    class="underline hover:no-underline"
-                                    :href="$t('about_project_more_url')"
-                                    target="_blank"
-                                    >{{ $t('about_project_more_label') }}</a
-                                >
-                            </p>
-                        </div>
-
-                        <img
-                            class="mt-8 max-w-[8rem]"
-                            src="../../img/eea_grants.png"
-                            alt="Iceland Lichtenstein Norway grants"
-                        />
-                    </template>
-                </AboutCollapsible>
-                <AboutCollapsible class="scroll-mt-12 border-b-2 border-gray-softest">
-                    <template v-slot:summary>Vyrobil lab.SNG</template>
-                    <template v-slot:content>
                         <ul>
-                            <li>Gabriel Balkó: {{ $t('Production') }}</li>
-                            <li>Michal Čudrnák: {{ $t('Project lead') }}</li>
-                            <li>Dominika Čupková: {{ $t('Video') }}</li>
-                            <li>Romana Halgošová: {{ $t('Video') }}</li>
-                            <li>Rastislav Chynoranský: {{ $t('Development') }}</li>
-                            <li>Zuzana Koblišková: {{ $t('Content, English translation') }}</li>
-                            <li>Michaela Kováčová: {{ $t('Content') }}</li>
-                            <li>Igor Rjabinin: {{ $t('Development') }}</li>
-                            <li>Filip Ruisl: {{ $t('UX design, graphic design') }}</li>
-                            <li>František Sebestyén: {{ $t('Development') }}</li>
-                            <li>Lukáš Štepanovský: {{ $t('Content, UX design, concept') }}</li>
-                            <li>Jana Šuchová: {{ $t('Artist research') }}</li>
-                            <li>Barbora Tribulová: {{ $t('Content') }}</li>
-                            <li>Katarína Vass: {{ $t('User research') }}</li>
-                            <li>Karin Vicianová: {{ $t('Content') }}</li>
-                            <li>Ernest Walzel: {{ $t('Development') }}</li>
+                            <li>Prihláste sa na SNG WIFI (heslo: welcometosng)</li>
+                            <li>Vyberte si jednu z výstav a preskúmajte diela, na ktoré počas návštevy narazíte.</li>
+                            <li>Kód pri diele vyťukajte do mriežky v aplikácii.</li>
+                            <li>
+                                Diela si môžete uložiť do svojej kolekcie a poslať na e-mail a vrátiť sa k nim doma s
+                                rozšírenými informáciami z archívu.
+                            </li>
                         </ul>
+
+                        <p class="my-4">
+                            Ester vám pri putovaní výstavou ponúkne videá zo zákulisia a originálne pohľady na diela od
+                            vystavujúcich osobností, alebo návštevníkov a návštevníčok.
+                        </p>
                     </template>
+                </AboutCollapsible>
+                <AboutCollapsible class="border-b-1 border-gray-softest">
+                    <template v-slot:summary>Projekt</template>
+                    <template v-slot:content></template>
                 </AboutCollapsible>
             </div>
         </div>
@@ -96,6 +51,14 @@
 </template>
 
 <style scoped>
+ul {
+    @apply ml-6 list-disc;
+}
+
+li {
+    @apply pl-2.5;
+}
+
 .v-enter-from > div,
 .v-leave-to > div {
     @apply -translate-x-full;
