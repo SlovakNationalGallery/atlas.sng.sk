ab08dc75a405f26a7d3984d737fbb55dab6dc293
Igor Rjabinin <igor.rjabinin@gmail.com>
2023-01-28 23:59:07 +0100

    include media in import places

--- app/Jobs/ImportPlacesJob.php
+++ app/Jobs/ImportPlacesJob.php
@@ -32,6 +32,44 @@ class ImportPlacesJob implements ShouldQueue
         DB::transaction(function () use ($places) {
             Place::whereNotIn('id', $places->pluck('id'))->delete();
             Place::upsert($places->map->except(['media'])->toArray(), ['id']);
+
+            $places->each(function ($upstreamPlace) {
+                $place = Place::find($upstreamPlace['id']);
+
+                $importedAirtableIds = $place
+                    ->media()
+                    ->pluck('custom_properties')
+                    ->pluck('airtable_id');
+
+                $upstreamAirtableIds = collect($upstreamPlace['media'])->pluck('id');
+
+                if ($importedAirtableIds == $upstreamAirtableIds) {
+                    return;
+                }
+
+                // Inserts
+                collect($upstreamPlace['media'])
+                    ->reject(fn ($media) => $importedAirtableIds->contains($media['id']))
+                    ->each(function ($media) use ($place) {
+                        $place
+                            ->addMediaFromUrl($media['url'])
+                            ->withCustomProperties([
+                                'airtable_id' => $media['id'],
+                            ])
+                            ->withResponsiveImages()
+                            ->toMediaCollection();
+                    });
+
+                // Sorting
+                $upstreamAirtableIds->each(function ($airtableId, $index) use ($place) {
+                    $place
+                        ->media()
+                        ->where('custom_properties->airtable_id', $airtableId)
+                        ->update([
+                            'order_column' => $index + 1, // medialibrary sorts from 1 by default
+                        ]);
+                });
+            });
         });
     }
 }
