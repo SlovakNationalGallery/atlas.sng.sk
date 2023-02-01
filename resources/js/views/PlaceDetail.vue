7f617f8ee81b3200a548ac7c3d3397e8f4e92918
Igor Rjabinin <igor.rjabinin@gmail.com>
2023-02-01 13:27:28 +0100

    add translation strings + code cleanup

--- resources/js/views/PlaceDetail.vue
+++ resources/js/views/PlaceDetail.vue
@@ -20,7 +20,6 @@
             <img class="h-9 w-9" :src="`/img/${place.code}.svg`" :alt="place.code" />
         </div>
         <h2 class="text-1.5xl font-bold">{{ place.title }}</h2>
-        <div>aha: {{ route.query.edit }}</div>
         <div class="my-4 space-y-4" v-html="place.description"></div>
         <Collapsible v-if="place.video_embed" :open="true" class="my-4">
             <template v-slot:summary>
