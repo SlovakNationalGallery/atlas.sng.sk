dc3a6b81befec3799f49b0e158ae16911951ccce
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2023-04-26 11:00:31 +0700

    bucketlist link in item detail

--- resources/js/views/ItemDetail.vue
+++ resources/js/views/ItemDetail.vue
@@ -1,94 +1,122 @@
 <template>
-    <div class="relative w-full border-b-2 border-black bg-gray-softest" v-if="item">
-        <ImageMovable
-            v-if="route.query.edit"
-            :alt="`${item.author}: ${item.title}`"
-            :src="item.image_src"
-            :srcset="item.image_srcset"
-            :offset-top="item.offset_top"
-        />
-        <ImageLightbox
-            v-else
-            :alt="`${item.author}: ${item.title}`"
-            :src="item.image_src"
-            :srcset="item.image_srcset"
-            :offset-top="item.offset_top"
-        />
-    </div>
-    <div class="relative h-full border-black px-4 pb-24 pt-8" v-if="item">
-        <div class="absolute -top-8 inline-block rounded-md bg-black p-1.5" v-if="item.code">
-            <img class="h-9 w-9" :src="`/img/${item.code}.svg`" :alt="item.code" />
+    <div v-if="item">
+        <article class="bg-green/20 p-4 pb-5" v-if="bucketlist">
+            <h3 class="text-1.5xl font-medium">{{ $t('You’ve found a hidden artwork!') }}</h3>
+            <p class="mt-2 leading-snug">{{ $t('Keep collecting hidden artworks to unlock a bonus story!') }}</p>
+            <router-link :to="{ name: 'my_collection' }" class="mt-3 block">
+                <Thumbnail class="bg-white">
+                    <template #image>
+                        <div class="flex h-full items-center justify-center bg-green">
+                            <SvgQuestion />
+                        </div>
+                    </template>
+                    <template #title>{{ $t('Hidden artworks') }}</template>
+                    <template #description
+                        >{{ found.length }}/{{ bucketlist.items.length }} collected. Tap to show the full list</template
+                    >
+                </Thumbnail>
+            </router-link>
+        </article>
+        <div class="relative w-full border-b-2 border-black bg-gray-softest">
+            <ImageMovable
+                v-if="route.query.edit"
+                :alt="`${item.author}: ${item.title}`"
+                :src="item.image_src"
+                :srcset="item.image_srcset"
+                :offset-top="item.offset_top"
+            />
+            <ImageLightbox
+                v-else
+                :alt="`${item.author}: ${item.title}`"
+                :src="item.image_src"
+                :srcset="item.image_srcset"
+                :offset-top="item.offset_top"
+            />
         </div>
-        <h2 class="text-1.5xl font-bold">{{ item.title }}</h2>
-        <h3 class="text-lg text-gray-dark">{{ item.author }} · {{ item.dating }}</h3>
-        <div class="text-lg text-gray-dark" v-if="item.location_formatted">
-            {{ $t('Location') }}: {{ item.location_formatted }}
+        <div class="relative h-full border-black px-4 pb-24 pt-8">
+            <div class="absolute -top-8 inline-block rounded-md bg-black p-1.5" v-if="item.code">
+                <img class="h-9 w-9" :src="`/img/${item.code}.svg`" :alt="item.code" />
+            </div>
+            <h2 class="text-1.5xl font-bold">{{ item.title }}</h2>
+            <h3 class="text-lg text-gray-dark">{{ item.author }} · {{ item.dating }}</h3>
+            <div class="text-lg text-gray-dark" v-if="item.location_formatted">
+                {{ $t('Location') }}: {{ item.location_formatted }}
+            </div>
+            <div class="my-4 space-y-4" v-html="item.description"></div>
+            <Collapsible :open="i === 0" v-for="(authority, i) in item.authorities" :key="authority.id" class="my-4">
+                <template v-slot:summary>
+                    <AuthoritySummary :authority="authority" />
+                </template>
+                <template v-slot:content v-if="authority.biography">
+                    <AuthorityDetails :authority="authority" />
+                </template>
+            </Collapsible>
+            <Collapsible v-if="!item.authorities.length && item.author_description" :open="true" class="my-4">
+                <template v-slot:summary>
+                    <AuthorSummary :item="item" />
+                </template>
+                <template v-slot:content>
+                    <AuthorDetails :item="item" />
+                </template>
+            </Collapsible>
+            <Collapsible v-if="item.video_embed" class="my-4">
+                <template v-slot:summary>
+                    <VideoSummary
+                        :thumbnail="item.video_thumbnail"
+                        :title="item.video_title"
+                        :subtitle="item.video_subtitle"
+                    />
+                </template>
+                <template v-slot:content>
+                    <ResponsiveVideoEmbed
+                        :src="item.video_embed"
+                        :width="item.video_aspect_ratio.width"
+                        :height="item.video_aspect_ratio.height"
+                    />
+                </template>
+            </Collapsible>
+            <StoryButton :storyId="item.story_id" class="my-4" v-if="item.story_id" />
+            <WebumeniaButton :url="item.webumenia_url" class="my-4" />
         </div>
-        <div class="my-4 space-y-4" v-html="item.description"></div>
-        <Collapsible :open="i === 0" v-for="(authority, i) in item.authorities" :key="authority.id" class="my-4">
-            <template v-slot:summary>
-                <AuthoritySummary :authority="authority" />
-            </template>
-            <template v-slot:content v-if="authority.biography">
-                <AuthorityDetails :authority="authority" />
-            </template>
-        </Collapsible>
-        <Collapsible v-if="!item.authorities.length && item.author_description" :open="true" class="my-4">
-            <template v-slot:summary>
-                <AuthorSummary :item="item" />
-            </template>
-            <template v-slot:content>
-                <AuthorDetails :item="item" />
-            </template>
-        </Collapsible>
-        <Collapsible v-if="item.video_embed" class="my-4">
-            <template v-slot:summary>
-                <VideoSummary
-                    :thumbnail="item.video_thumbnail"
-                    :title="item.video_title"
-                    :subtitle="item.video_subtitle"
-                />
-            </template>
-            <template v-slot:content>
-                <ResponsiveVideoEmbed
-                    :src="item.video_embed"
-                    :width="item.video_aspect_ratio.width"
-                    :height="item.video_aspect_ratio.height"
-                />
-            </template>
-        </Collapsible>
-        <StoryButton :storyId="item.story_id" class="my-4" v-if="item.story_id" />
-        <WebumeniaButton :url="item.webumenia_url" class="my-4" />
     </div>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+import { useBucketlistStore } from '../stores/BucketlistStore'
 import { useItemStore } from '../stores/ItemStore'
 import AuthorityDetails from '../components/AuthorityDetails.vue'
 import AuthoritySummary from '../components/AuthoritySummary.vue'
 import Collapsible from '../components/Collapsible.vue'
-import ConfirmButton from '../components/ConfirmButton.vue'
 import ImageLightbox from '../components/ImageLightbox.vue'
 import ImageMovable from '../components/ImageMovable.vue'
 import WebumeniaButton from '../components/WebumeniaButton.vue'
 import StoryButton from '../components/StoryButton.vue'
 import VideoSummary from '../components/VideoSummary.vue'
-import HistoryBack from '../components/HistoryBack.vue'
 import ResponsiveVideoEmbed from '../components/ResponsiveVideoEmbed.vue'
-import SvgBack from '../components/svg/Back.vue'
-import SvgHeartSmall from '../components/svg/HeartSmall.vue'
 import AuthorSummary from '../components/AuthorSummary.vue'
 import AuthorDetails from '../components/AuthorDetails.vue'
+import Thumbnail from '../components/Thumbnail.vue'
+import SvgQuestion from '../components/svg/Question.vue'
 
 const route = useRoute()
+const bucketlistStore = useBucketlistStore()
 const itemStore = useItemStore()
 const item = ref(null)
+const bucketlist = ref(null)
+const found = computed(() => bucketlist.value.items.filter((item) => itemStore.isViewed(item.id)))
 
 onMounted(async () => {
     const id = route.params.id
     item.value = await itemStore.load(id)
     itemStore.addViewed(id)
+    const defaultBucketlist = item.value.bucketlists.find(
+        (bucketlist) => bucketlist.id === import.meta.env.VITE_DEFAULT_BUCKETLIST
+    )
+    if (defaultBucketlist) {
+        bucketlist.value =
+            bucketlistStore.get(defaultBucketlist.id) || (await bucketlistStore.load(defaultBucketlist.id))
+    }
 })
 </script>
