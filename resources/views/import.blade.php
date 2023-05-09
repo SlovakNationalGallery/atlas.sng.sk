841cdf6149a6f51a575d554c15c58b6bfbc4da27
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2023-05-10 01:32:33 +0700

    add import link

--- resources/views/import.blade.php
+++ resources/views/import.blade.php
@@ -10,6 +10,7 @@
 
         <div class="pl-8">
             <a class="underline hover:no-underline" target="_blank" href="{{ route('import_type', ['type' => 'sections']) }}">sections (w/ items, exhibitions, stories, locations)</a><br />
+            <a class="underline hover:no-underline" target="_blank" href="{{ route('import_type', ['type' => 'bucketlists']) }}">bucketlists (w/ items, exhibitions, stories, locations)</a><br />
             <a class="underline hover:no-underline" target="_blank" href="{{ route('import_type', ['type' => 'items']) }}">items (w/ exhibitions, stories, locations)</a><br />
             <a class="underline hover:no-underline" target="_blank" href="{{ route('import_type', ['type' => 'places']) }}">places (w/ exhibitions, stories, locations)</a><br />
             <a class="underline hover:no-underline" target="_blank" href="{{ route('import_type', ['type' => 'exhibitions']) }}">exhibitions</a><br />
