7d2349564074613b1844df5a824530c6e033ee7e
Igor Rjabinin <igor.rjabinin@gmail.com>
2023-06-28 13:48:48 +0200

    use syncMedia in ImportPlacesJob and ImportStoriesJob

--- app/Jobs/ImportPlacesJob.php
+++ app/Jobs/ImportPlacesJob.php
@@ -4,7 +4,6 @@ namespace App\Jobs;
 
 use App\Models\Place;
 use App\Models\Exhibition;
-use Illuminate\Support\Arr;
 use Illuminate\Bus\Queueable;
 use Illuminate\Support\Facades\DB;
 use App\DataMappers\AirtableMapper;
@@ -12,11 +11,11 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
+use App\Traits\SyncsMedia;
 
 class ImportPlacesJob implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels, SyncsMedia;
 
     /**
      * Execute the job.
@@ -31,8 +30,8 @@ class ImportPlacesJob implements ShouldQueue
 
         $places = \Airtable::table('places')
             ->all()
-            ->pipe(fn($places) => $mapper->mapTable($places, 'places', false));
-            
+            ->pipe(fn ($places) => $mapper->mapTable($places, 'places', false));
+
         DB::transaction(function () use ($places, $exhibition_ids) {
             $missing_ids = Place::whereNotIn('id', $places->pluck('id'))
                 ->get()
@@ -52,39 +51,8 @@ class ImportPlacesJob implements ShouldQueue
                     $place->code->save();
                 }
 
-                $importedAirtableIds = $place
-                    ->media()
-                    ->pluck('custom_properties')
-                    ->pluck('airtable_id');
-
-                $upstreamAirtableIds = collect($upstreamPlace['media'])->pluck('id');
-
-                if ($importedAirtableIds == $upstreamAirtableIds) {
-                    return;
-                }
-
-                // Inserts
-                collect($upstreamPlace['media'])
-                    ->reject(fn($media) => $importedAirtableIds->contains($media['id']))
-                    ->each(function ($media) use ($place) {
-                        $place
-                            ->addMediaFromUrl($media['url'])
-                            ->withCustomProperties([
-                                'airtable_id' => $media['id'],
-                            ])
-                            ->withResponsiveImages()
-                            ->toMediaCollection();
-                    });
-
-                // Sorting
-                $upstreamAirtableIds->each(function ($airtableId, $index) use ($place) {
-                    $place
-                        ->media()
-                        ->where('custom_properties->airtable_id', $airtableId)
-                        ->update([
-                            'order_column' => $index + 1, // medialibrary sorts from 1 by default
-                        ]);
-                });
+                // sync media
+                $this->syncMedia($place, $upstreamPlace);
             });
         });
     }
