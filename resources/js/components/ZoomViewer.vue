4239624a6ef0a75e4f3ec1be968fb959d3e85f1f
FrantisekMichalSebestyen <sebestyen.frantisek.michal@gmail.com>
2023-07-27 13:07:03 +0200

    feat: add zoom on images

---
+++ resources/js/components/ZoomViewer.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="viewer" class="absolute inset-0 rounded-xl overflow-hidden"></div>
+</template>
+
+<script setup>
+import OpenSeaDragon from 'openseadragon'
+import { onMounted, onBeforeUnmount, ref } from 'vue'
+const props = defineProps(['tileSources'])
+const viewer = ref(null)
+//TODO: navigation for multiple images
+
+onMounted(() => {
+    viewer.value = OpenSeaDragon({
+        id: 'viewer',
+        tileSources: props.tileSources[0].deep_zoom_url,
+        showNavigationControl: false,
+        showNavigator: false,
+    })
+})
+
+onBeforeUnmount(() => {
+    if (viewer.value) {
+        viewer.value.destroy()
+    }
+})
+
+</script>
