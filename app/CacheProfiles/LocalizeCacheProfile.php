169411e1f9e6b64f97d8ad2a38bb5346c84f4d6f
Igor Rjabinin <igor.rjabinin@gmail.com>
2023-12-14 13:42:41 +0100

    add LocalizeCacheProfile to include x-locale in cache hash

---
+++ app/CacheProfiles/LocalizeCacheProfile.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\CacheProfiles;
+
+use Spatie\ResponseCache\CacheProfiles\CacheAllSuccessfulGetRequests;
+use Illuminate\Http\Request;
+
+class LocalizeCacheProfile extends CacheAllSuccessfulGetRequests
+{
+    public function useCacheNameSuffix(Request $request): string
+    {
+        return $request->header('X-locale', app()->getLocale());        
+    }
+}
